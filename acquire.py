f2ee66b59c85bfbf572615774abcb8b80d606bf6
lupeluna <80718680+lupeluna@users.noreply.github.com>
2021-09-03 09:58:53 -0500

    update functions

--- acquire.py
+++ acquire.py
@@ -3,6 +3,25 @@ import numpy as np
 import os
 import requests
 
+#acquire
+#from env import host, user, password
+from pydataset import data
+
+
+# turn off pink warning boxes
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# get helper function to get the necessary connction to url.
+
+# def get_connection(db, user=user, host=host, password=password):
+#     '''
+#     This function uses my info from my env file to create a connection urs to access database info.
+#     '''
+#     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+    
+
 def get_df():
     '''
     returns Water Potability data into a csv, and creates it for you
